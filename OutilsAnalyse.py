e8ce26deab0ad38ce52611cba664563912102f56
Jessy <166802675+Jess220504@users.noreply.github.com>
2024-05-07 17:53:44 +0200

    Assemblage des deux codes

--- OutilsAnalyse.py
+++ OutilsAnalyse.py
@@ -3,13 +3,14 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from Point import Point
 from GroupeDePoints import GroupeDePoints
+from sklearn.cluster import KMeans
 
 class OutilsAnalyse:
     
     def __init__(self):
         print("")
         
-    def convertirPointsEnListe(self,grp): #Fonction qui convertit un groupeDePoints en Liste pour l'utiliser dans d'autres fonctions
+    def convertirPointsEnListe(self,grp): #Fonction qui convertit un GroupeDePoints en Liste pour l'utiliser dans d'autres fonctions
         grpDePoints=[]
         for point in grp:
             grpDePoints.append([point.getX(),point.getY()])
@@ -37,8 +38,17 @@ class OutilsAnalyse:
         return
 
     
-    def Kmean():
-        return
+    def kMeans(self,nbCluster,grp): #Algorithme des KMeans qui prend un nombre de cluster et un groupe de points en paramètre et qui renvoie plusieurs groupes de points (clusters)
+        grpDePoints=self.convertirPointsEnListe(grp)
+        kmeans=KMeans(n_clusters=nbCluster,random_state=0,n_init="auto").fit(grpDePoints).labels_ #effectue l'algorithme des KMeans
+        resultat=[]
+        for i in range(nbCluster): #création des clusters à renvoyer
+            resultat.append(GroupeDePoints(str(i),[]))
+        for i in range(len(kmeans)): #ajoute chaque point au cluster auquel il appartient selon l'algorithme des KMeans
+            for j in range(nbCluster):
+                if resultat[j].getNom()==str(kmeans[i]):
+                    resultat[j].ajoutePoint(grp[i])
+        return resultat
     
     def prediction():
         return
