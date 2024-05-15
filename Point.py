93733acd753b7c4d541fd3760d19c2b2735b6a59
Keriann Chauvin <kchauvin@u20focal049.univ-lr.fr>
2024-05-15 16:35:44 +0200

    Modification de la classe point pour ajouté la variable catéguorielle typePlanete, on en a besoin pour les predictions

--- Point.py
+++ Point.py
@@ -1,16 +1,21 @@
 class Point: #définition de la classe Point et initialisation de toutes ses variables
     nom=""
     coord=[]
+    typePlanete=""
 
-    def __init__(self,nom,coordonnees): #définition du constructeur de la classe Point
+    def __init__(self,nom,coordonnees,typePlanete=""): #définition du constructeur de la classe Point
         self.nom=nom
         self.coord=coordonnees
+        self.typePlanete = typePlanete
 
     def getNom(self):
         return self.nom
 
     def getCoord(self): #retourne les coordonnées du point
         return self.coord
+    
+    def getTypePlanete(self):
+        return self.typePlanete
     
     def toString(self):
         return self.nom+""+str(self.coord)
