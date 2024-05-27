069f627d2b0c1d6f14ab7fd6c4d160fe54aca1f4
Jessy <166802675+Jess220504@users.noreply.github.com>
2024-05-27 14:48:57 +0200

    Ajout de commentaires

--- OutilsAnalyse.py
+++ OutilsAnalyse.py
@@ -39,7 +39,7 @@ class OutilsAnalyse:
         #A modifier car doit prendre en compte les parametres d'un groupe de points
         return np.corrcoef(grp1, grp2)[0, 1]
     
-    def kMeans(self,nbCluster,grp): #Algorithme des KMeans qui prend un nombre de cluster et un groupe de points en paramètre et qui renvoie plusieurs groupes de points (clusters)
+    def kMeans(self,nbCluster,grp): #Algorithme des KMeans qui prend un nombre de cluster et un groupe de points en paramètre et qui renvoie une liste de groupes de points (clusters)
         grpDePoints=self.convertirPointsEnListe(grp)
         kmeans=KMeans(n_clusters=nbCluster,random_state=0,n_init="auto").fit(grpDePoints).labels_ #effectue l'algorithme des KMeans
         resultat=[]
@@ -47,12 +47,11 @@ class OutilsAnalyse:
             resultat.append(GroupeDePoints(str(i),[]))
         for i in range(len(kmeans)): #ajoute chaque point au cluster auquel il appartient selon l'algorithme des KMeans
             for j in range(nbCluster):
-                if resultat[j].getNom()==str(kmeans[i]):
-                    resultat[j].ajoutePoint(grp.getPoint(i))
+                if resultat[j].getNom()==str(kmeans[i]): #si le nom du cluster = nom du cluster à l'indice du point
+                    resultat[j].ajoutePoint(grp.getPoint(i)) #ajouter le point au bon cluster
         return resultat
     
-    #nbVoisin : int, grpApprentissage : groupeDePoints, grpPrediction : groupeDePoints
-    def predictionKNN(self, nbVoisin, grpApprentissage, grpPrediction):
+    def predictionKNN(self, nbVoisin, grpApprentissage, grpPrediction): #nbVoisin : int, grpApprentissage : groupeDePoints, grpPrediction : groupeDePoints
         resultat = GroupeDePoints("Groupe de point prediction Knn",) #Créer un groupe de point vide
         for i in range(0, grpPrediction.getNbPoints()): #Parcours la lsite des points qu'on veut prédire
             copieDuPoint = grpPrediction.getPoints()[i].copiePoint() #Copie ces points pour ne pas écraser ceux fournies si jamais on veut les traiter
